a1d55f69e5080e24e2ff88db917165c0e3f7d5bb
Christopher Su <christophersu9@gmail.com>
2014-10-26 15:50:31 -0700

    vowels

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 $(function() {
     var products = ['alarm clock', 'television', 'microwave', 'keyboard', 'water bottle', 'eBook reader', 'phone', 'chair', 'trash can', 'stopwatch', 'wallet'];
     var constraints = ['for the visually impaired', 'for the elderly', 'for children', 'for families with children', 'for the hearing impaired', 'for the speech impaired'];
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
     $("#prompt-btn").click(function() {
         var product = products[Math.floor(Math.random()*products.length)];
-        var vowels = ['a', 'e', 'i', 'o', 'u'];
         var prefix;
-        if (vowels.contains(product.charAt(0)))
+        if (vowels.indexOf(product.charAt(0)) !== -1) {
             prefix = "Design an ";
-        else
+        }
+        else {
             prefix = "Design a ";
+        }
         $("#prompt").text(prefix + product + " " + constraints[Math.floor(Math.random()*constraints.length)] + ".");
         $(this).text("Get another prompt!");
     });
