a70c866a3f11ed7b8eee1207a95bd343ff6316a0
Christopher Su <christophersu9@gmail.com>
2014-10-26 15:51:18 -0700

    products

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 $(function() {
-    var products = ['alarm clock', 'television', 'microwave', 'keyboard', 'water bottle', 'eBook reader', 'phone', 'chair', 'trash can', 'stopwatch', 'wallet'];
+    var products = ['alarm clock', 'television', 'microwave', 'keyboard', 'water bottle', 'eBook reader', 'phone', 'chair', 'trash can', 'stopwatch', 'wallet', 'keychain', 'bicycle'];
     var constraints = ['for the visually impaired', 'for the elderly', 'for children', 'for families with children', 'for the hearing impaired', 'for the speech impaired'];
     var vowels = ['a', 'e', 'i', 'o', 'u'];
     $("#prompt-btn").click(function() {
