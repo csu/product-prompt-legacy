56b4f745161c11fa6128387ba3d29f94190436cc
Christopher Su <christophersu9@gmail.com>
2014-10-26 15:30:19 -0700

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,10 @@ var gulp = require('gulp'),
     rename = require("gulp-rename"),
     uglify = require('gulp-uglify');
 
-gulp.task('index', function() {
-    gulp.src('./src/index.html')
-        .pipe(minifyHTML())
-        .pipe(rename('index.html'))
-        .pipe(gulp.dest('.'))
-    gulp.src('./src/index.css')
-        .pipe(minifyCSS())
-        .pipe(rename('index.css'))
-        .pipe(gulp.dest('.'))
-    gulp.src('./src/index.js')
-        .pipe(uglify())
-        .pipe(rename('index.js'))
-        .pipe(gulp.dest('.'))
-});
+gulp.task('default', ['index']);
+gulp.task('build', ['index']);
 
-gulp.task('build', function() {
+gulp.task('index', function() {
     gulp.src('./src/index.html')
         .pipe(minifyHTML())
         .pipe(rename('index.html'))
