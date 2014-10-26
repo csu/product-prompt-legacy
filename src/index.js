77b05fb079930d6b4bac0da922ad1a8490ab36f2
Christopher Su <christophersu9@gmail.com>
2014-10-26 15:48:41 -0700

    vowels

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ $(function() {
     var constraints = ['for the visually impaired', 'for the elderly', 'for children', 'for families with children', 'for the hearing impaired', 'for the speech impaired'];
     $("#prompt-btn").click(function() {
         var product = products[Math.floor(Math.random()*products.length)];
+        var vowels = ['a', 'e', 'i', 'o', 'u'];
         var prefix;
-        if (product.charAt(0) == 'a')
+        if (vowels.contains(product.charAt(0)))
             prefix = "Design an ";
         else
             prefix = "Design a ";
