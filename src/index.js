be8d7eabd45269d67385a507e8da78df2aa641b7
Christopher Su <christophersu9@gmail.com>
2014-10-27 11:18:38 -0700

    constraints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 $(function() {
     var products = ['alarm clock', 'television', 'microwave', 'keyboard', 'water bottle', 'eBook reader', 'phone', 'chair', 'trash can', 'stopwatch', 'wallet', 'keychain', 'bicycle', 'binder', 'notebook'];
-    var constraints = ['for the visually impaired', 'for the elderly', 'for children', 'for families with children', 'for the hearing impaired', 'for the speech impaired', 'for astronauts', 'for scuba divers'];
+    var constraints = ['for the visually impaired', 'for the elderly', 'for children', 'for families with children', 'for the hearing impaired', 'for the speech impaired', 'for astronauts'];
     var vowels = ['a', 'e', 'i', 'o', 'u'];
     $("#prompt-btn").click(function() {
         var product = products[Math.floor(Math.random()*products.length)];
